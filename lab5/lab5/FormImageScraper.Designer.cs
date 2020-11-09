91acf4499928486fe8f6a1ce7214649ff1cbd4c0
philipjakobsson <71271470+philipjakobsson@users.noreply.github.com>
2020-11-09 14:13:23 +0100

    Add files via upload

---
+++ lab5/lab5/FormImageScraper.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace lab5
+{
+    partial class FormImageScraper
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxURI = new System.Windows.Forms.TextBox();
+            this.buttonExtract = new System.Windows.Forms.Button();
+            this.labelImagesFound = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.listBoxMatches = new System.Windows.Forms.ListBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // textBoxURI
+            // 
+            this.textBoxURI.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxURI.Location = new System.Drawing.Point(12, 12);
+            this.textBoxURI.Name = "textBoxURI";
+            this.textBoxURI.Size = new System.Drawing.Size(430, 20);
+            this.textBoxURI.TabIndex = 0;
+            this.textBoxURI.Text = "https://archillect.com/";
+            // 
+            // buttonExtract
+            // 
+            this.buttonExtract.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonExtract.Location = new System.Drawing.Point(448, 10);
+            this.buttonExtract.Name = "buttonExtract";
+            this.buttonExtract.Size = new System.Drawing.Size(75, 23);
+            this.buttonExtract.TabIndex = 1;
+            this.buttonExtract.Text = "&Extract";
+            this.buttonExtract.UseVisualStyleBackColor = true;
+            this.buttonExtract.Click += new System.EventHandler(this.buttonExtract_Click);
+            // 
+            // labelImagesFound
+            // 
+            this.labelImagesFound.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelImagesFound.AutoSize = true;
+            this.labelImagesFound.Location = new System.Drawing.Point(9, 431);
+            this.labelImagesFound.Name = "labelImagesFound";
+            this.labelImagesFound.Size = new System.Drawing.Size(103, 13);
+            this.labelImagesFound.TabIndex = 3;
+            this.labelImagesFound.Text = "Images found: None";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSave.Enabled = false;
+            this.buttonSave.Location = new System.Drawing.Point(447, 426);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "&Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // listBoxMatches
+            // 
+            this.listBoxMatches.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxMatches.FormattingEnabled = true;
+            this.listBoxMatches.Location = new System.Drawing.Point(12, 38);
+            this.listBoxMatches.Name = "listBoxMatches";
+            this.listBoxMatches.Size = new System.Drawing.Size(510, 381);
+            this.listBoxMatches.TabIndex = 5;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(121, 426);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(320, 23);
+            this.progressBar.TabIndex = 6;
+            // 
+            // folderBrowserDialog
+            // 
+            this.folderBrowserDialog.SelectedPath = "%userprofile%\\Desktop";
+            // 
+            // FormImageScraper
+            // 
+            this.AcceptButton = this.buttonExtract;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 461);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.listBoxMatches);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.labelImagesFound);
+            this.Controls.Add(this.buttonExtract);
+            this.Controls.Add(this.textBoxURI);
+            this.MinimumSize = new System.Drawing.Size(550, 500);
+            this.Name = "FormImageScraper";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ImageScraper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxURI;
+        private System.Windows.Forms.Button buttonExtract;
+        private System.Windows.Forms.Label labelImagesFound;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.ListBox listBoxMatches;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+    }
+}
+
---
+++ lab5/lab5/FormImageScraper.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace lab5
+{
+    public partial class FormImageScraper : Form
+    {
+        private HttpClient client = new HttpClient();
+        private Stopwatch stopwatch = new Stopwatch();
+        private Dictionary<Task<byte[]>, string> allImageBytes = new Dictionary<Task<byte[]>, string>();
+       
+        private int totalDownloaded;
+        private int failedDownload;
+
+        public FormImageScraper()
+        {
+            InitializeComponent();
+        }
+
+        private async void buttonExtract_Click(object sender, EventArgs e)
+        {
+            listBoxMatches.Items.Clear();
+
+            string pattern = @"(?<=<img.*src="")[^""]+(?="".*)";
+
+            string inputURI = textBoxURI.Text;
+
+            if (!textBoxURI.Text.StartsWith("http"))
+            {
+                inputURI = "http://" + textBoxURI.Text;
+            } 
+
+            string website = await client.GetStringAsync(inputURI);
+
+            MatchCollection matches = Regex.Matches(website, pattern, RegexOptions.IgnoreCase);
+
+            foreach (Match match in matches)
+            {
+                if (!match.Value.StartsWith("http", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    listBoxMatches.Items.Add(inputURI + match.Value);
+                }
+                else
+                {
+                    listBoxMatches.Items.Add(match.Value);
+                }
+            }
+
+            if (listBoxMatches.Items.Count != 0)
+            {
+                buttonSave.Enabled = true;
+                progressBar.Maximum = listBoxMatches.Items.Count;
+                labelImagesFound.Text = $"Images found: {listBoxMatches.Items.Count}";
+            }
+        }
+        private async void buttonSave_Click(object sender, EventArgs e)
+        {
+            stopwatch.Restart();
+            progressBar.Value = 0;
+            totalDownloaded = 0;
+            failedDownload = 0;
+
+            if (listBoxMatches.Items.Count == 0)
+            {
+                MessageBox.Show(this, "Could not find images.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return;
+            }
+            else if (folderBrowserDialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            string directory = folderBrowserDialog.SelectedPath;
+
+            Directory.CreateDirectory(directory);
+
+            foreach (string URL in listBoxMatches.Items)
+            {
+                Task<byte[]> imageTask = client.GetByteArrayAsync(URL);
+
+                allImageBytes.Add(imageTask, URL);
+            }
+
+            await ProcessTasks(directory);
+        }
+
+        private async Task ProcessTasks(string directory)
+        {
+            while (allImageBytes.Count > 0)
+            {
+                Task<byte[]> completedTask = null;
+                    
+                try
+                {
+                    completedTask = await Task.WhenAny(allImageBytes.Keys);
+
+                    Uri fileURL = new Uri(allImageBytes[completedTask]);    
+
+                    using (FileStream fileStream = File.Open(Path.Combine(directory, totalDownloaded + Path.GetFileName(fileURL.AbsolutePath)), FileMode.Create))
+                    {
+                        await fileStream.WriteAsync(completedTask.Result, 0, completedTask.Result.Length);
+
+                        if (completedTask.Status == TaskStatus.RanToCompletion)
+                        {
+                            totalDownloaded++;
+                        }
+                        else
+                        {
+                            failedDownload++;
+                        }
+
+                        IncrementProgressBar();
+                    }
+
+                    allImageBytes.Remove(completedTask);
+                }
+                catch (Exception e)
+                {
+                    
+                }
+                finally
+                {
+                    if (completedTask != null)
+                    {
+                        allImageBytes.Remove(completedTask);
+                    }
+                }
+            }
+        }
+        private void IncrementProgressBar()
+        {
+            progressBar.Value++;
+
+            if (progressBar.Value == listBoxMatches.Items.Count)
+            {
+                if (failedDownload > 0)
+                {
+                    MessageBox.Show(this, $"{totalDownloaded} images downloaded\n" +
+                        $"{failedDownload} images failed to download." +
+                    $"Total time: {stopwatch.Elapsed.TotalSeconds:F2} seconds.", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show(this, $"{totalDownloaded} images downloaded\n" +
+                    $"Total time: {stopwatch.Elapsed.TotalSeconds:F2} seconds.", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                
+                buttonSave.Visible = true;
+                stopwatch.Stop();
+            }
+        }
+    }
+}
